0fa6b9d5a65add0da6a73b606cfb50bc0f292bd8
LILIANEsouza17 <lilimax.ise@gmail.com>
2025-01-31 19:37:06 +0000

    criei tipos de dados

--- DESAFIO CARTAS SUPER TRUNFO LILIANE.c
+++ DESAFIO CARTAS SUPER TRUNFO LILIANE.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 //desafio super trunfo cadastrar tema
 int main(){
-    printf ("DESAFIO SUPER TRUNFO:  TEMA PAÍSES\n");  
+    printf ("DESAFIO SUPER TRUNFO:  TEMA PAÍSES\n"); 
+//cadastrar cartas das cidades
+char ESTADOS[8]; //nomes e letras dos estados
+char CÓDIGO[4];//letras e numeros das cartas
+char CIDADES[32];//nomes das cidades
+int população;
+float área km;
+float pib;
+int números ponto turístico;
+
 
 }
