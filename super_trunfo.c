693c655f8c8e3fe81407e2a790399e7f5cd0fa54
LILIANEsouza17 <lilimax.ise@gmail.com>
2025-02-20 23:02:03 +0000

    Criei printf e scanf para o codigo da carta 1.

--- super_trunfo.c
+++ super_trunfo.c
@@ -8,7 +8,7 @@ int main(){
 
     char ESTADO [] = "ESTADO A,B,C,D,E,F,G,H";
     char CIDADE[20];
-    char CODIGO[] = "A 1,2,3,4, B 1,2,3,4, C 1,2,3,4, D 1,2,3,4, E 1,2,3,4, F 1,2,3,4, G 1,2,3,4, H 1,2,3,4";
+    char CODIGO[2] = "A 1,2,3,4, B 1,2,3,4, C 1,2,3,4, D 1,2,3,4, E 1,2,3,4, F 1,2,3,4, G 1,2,3,4, H 1,2,3,4";
     int POPULACAO;
     float AREAKM;
     float PIB;
@@ -21,6 +21,10 @@ int main(){
     //USUARIO CADASTRA NOME DA CIDADE DO PAÍS ESCOLHIDO
     printf("\n CIDADE:  ");
     scanf(" 19%s",CIDADE);
+
+    //USUARIO CADASTRA O CÓDIGO DA CARTA 1
+    printf("\n CÓDIGO DA CARTA: ");
+    scanf("1%s",CODIGO);
 
     
 }
