ba6bfa383f4768cfde83d901789a637de160d649
LILIANEsouza17 <lilimax.ise@gmail.com>
2025-02-20 21:29:38 +0000

    CRIEI printf com o tema do jogo e criei char e int

---
+++ super_trunfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("DESAFIO SUPER TRUNFO - PAÍSES \n");//CADASTRO DO TEMA DO JOGO
+    //CADASTRAR DADOS CARTA 1
+
+    printf("CARTA 1\n");
+
+    char ESTADO[] = "ESTADO A,B,C,D,E,F,G,H";
+    char CIDADE[] = "1,2,3,4";
+    char CODIGO[] = "A 1,2,3,4, B 1,2,3,4, C 1,2,3,4, D 1,2,3,4, E 1,2,3,4, F 1,2,3,4, G 1,2,3,4, H 1,2,3,4";
+    int POPULACAO;
+    float AREAKM;
+    float PIB;
+    int NUMEROPONTOSTURISTICOS;
+}
