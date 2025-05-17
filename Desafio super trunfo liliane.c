76622dbd0038b5a434c7d8dccd3bd1537ff25c4d
LILIANEsouza17 <lilimax.ise@gmail.com>
2025-05-17 20:04:32 +0000

    MUDANÇA NO CODIGO

--- Desafio super trunfo liliane.c
+++ Desafio super trunfo liliane.c
@@ -3,12 +3,8 @@
 int main(){
     printf("DESAFIO SUPER TRUNFO: PAÍSES\n");
 
- //Nome do país
-    printf(" PAÍS BRASIL\n");
-    //CADASTRAR CARTAS
-    printf("%d ESTADOS %d CIDADES", ESTADOS= 8, CIDADES=32);
+ 
     
-   
 
    
    
